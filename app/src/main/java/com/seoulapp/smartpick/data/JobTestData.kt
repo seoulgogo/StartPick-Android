fbf7ca7b9da22436ea53908646a218d2e26be5eb
chooeeh <chchgml10@gmail.com>
2019-09-04 20:40:09 +0900

    jobFragment

---
+++ app/src/main/java/com/seoulapp/smartpick/data/JobTestData.kt
@@ -0,0 +1,9 @@
+package com.seoulapp.smartpick.data
+
+data class JobTestData (
+        val title : String,
+        val location : String,
+        val content : String,
+        val detail : String
+)
+
--- app/src/main/java/com/seoulapp/smartpick/ui/JobFragment.kt
+++ app/src/main/java/com/seoulapp/smartpick/ui/JobFragment.kt
@@ -4,16 +4,24 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.seoulapp.smartpick.R
+import com.seoulapp.smartpick.adapter.JobTestAdapter
+import com.seoulapp.smartpick.adapter.TestAdapter
+import com.seoulapp.smartpick.data.JobTestData
+import com.seoulapp.smartpick.data.TestData
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_job.*
 
 
 class JobFragment : Fragment() {
 
     private lateinit var rootView: View
+    lateinit var jobViewTestAdpapter : JobTestAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -23,4 +31,29 @@ class JobFragment : Fragment() {
         return rootView
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setRecyclerView()
+    }
+
+    fun setRecyclerView() {
+        var testDataList: ArrayList<JobTestData> = ArrayList()
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 1", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 2", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 3", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 4", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 5", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 6", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 7", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 8", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 9", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+        testDataList.add(JobTestData("SNS 콘텐츠 크리에이터 10", "서울시창업지원센터", "콘텐츠", "저희 회사는 모두가 의견을 자유롭게 내는 수평적인 조직 구조입니다."))
+
+
+        jobViewTestAdpapter = JobTestAdapter(activity!!, testDataList)
+        fragment_job_recyclerview.adapter = jobViewTestAdpapter
+        fragment_job_recyclerview.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+
+    }
 }
