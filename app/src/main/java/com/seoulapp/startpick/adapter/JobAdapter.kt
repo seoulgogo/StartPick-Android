2b16506a4a1d24176159a3ed4d0808271f919fea
chooeeh <chchgml10@gmail.com>
2019-09-19 23:52:41 +0900

    09/19

--- app/src/main/java/com/seoulapp/startpick/adapter/JobAdapter.kt
+++ app/src/main/java/com/seoulapp/startpick/adapter/JobAdapter.kt
@@ -2,7 +2,6 @@ package com.seoulapp.startpick.adapter
 
 import android.content.Context
 import android.content.Intent
-import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +9,8 @@ import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
 import com.seoulapp.startpick.R
-import com.seoulapp.startpick.WithDetailActivity
 import com.seoulapp.startpick.data.JobData
-import com.seoulapp.startpick.ui.WriteActivity
+import com.seoulapp.startpick.ui.WithDetailActivity
 
 class JobAdapter(private val ctx : Context, private val dataList : ArrayList<JobData>) :
         RecyclerView.Adapter<JobAdapter.Holder>() {
