f7ff0ce505d11ec64c08e42f74118480762bd4cf
chooeeh <chchgml10@gmail.com>
2019-09-24 01:29:43 +0900

    09/24

--- app/src/main/java/com/seoulapp/startpick/adapter/SupportAdapter.kt
+++ app/src/main/java/com/seoulapp/startpick/adapter/SupportAdapter.kt
@@ -1,13 +1,16 @@
 package com.seoulapp.startpick.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.seoulapp.startpick.R
 import com.seoulapp.startpick.data.SupportData
+import com.seoulapp.startpick.ui.SupportDetailActivity
 
 class SupportAdapter(private val ctx : Context, private val dataList : ArrayList<SupportData>) : RecyclerView.Adapter<SupportAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): SupportAdapter.ViewHolder {
@@ -23,9 +26,16 @@ class SupportAdapter(private val ctx : Context, private val dataList : ArrayList
         holder.title_tv.text = dataList[position].title
         holder.center_tv.text = dataList[position].center
         holder.data_tv.text = dataList[position].date
+
+        /* 아이템 클릭 이벤트 */
+        holder.item.setOnClickListener {
+            var intent = Intent(ctx, SupportDetailActivity::class.java)
+            ctx.startActivity(intent)
+        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var item = itemView as RelativeLayout
         var title_tv = itemView.findViewById(R.id.tv_supportinner_title) as TextView
         var center_tv = itemView.findViewById(R.id.tv_supportinner_center) as TextView
         var data_tv = itemView.findViewById(R.id.tv_supportinner_date) as TextView
---
+++ app/src/main/java/com/seoulapp/startpick/ui/SupportDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.seoulapp.startpick.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.seoulapp.startpick.R
+
+class SupportDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_detail)
+    }
+}
