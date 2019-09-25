726f425548ed0bae2b1a2df413f2bdc12785fe5d
chooeeh <chchgml10@gmail.com>
2019-09-25 23:23:01 +0900

    09/25

--- app/src/main/java/com/seoulapp/startpick/ui/WithDetailActivity.kt
+++ app/src/main/java/com/seoulapp/startpick/ui/WithDetailActivity.kt
@@ -1,11 +1,17 @@
 package com.seoulapp.startpick.ui
 
+import android.app.AlertDialog
+import android.app.ProgressDialog.show
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.telecom.Call
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
 import com.seoulapp.startpick.R
 import kotlinx.android.synthetic.main.activity_with_detail.*
 
@@ -23,6 +29,10 @@ class WithDetailActivity : AppCompatActivity() {
         btn_back.setOnClickListener {
             finish()
         }
+        three_point.setOnClickListener {
+            showPopup(three_point)
+        }
+
 
 
     }
@@ -39,5 +49,38 @@ class WithDetailActivity : AppCompatActivity() {
             window.addFlags(flags)
         }
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    /** 팝업창 */
+    private fun showPopup(view: View) {
+        var popup = PopupMenu(this, view)
+        popup.inflate(R.menu.menu_main)
+
+        popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+
+            when (item!!.itemId) {
+                // 수정버튼 클릭시
+                R.id.menu_modified -> {
+
+
+                }
+                // 삭제버튼 클릭시
+                R.id.menu_delete -> {
+                    val builder = AlertDialog.Builder(this)
+                    val dialogView = layoutInflater.inflate(R.layout.custom_dialog, null)
+                    val dialogNo = dialogView.findViewById<TextView>(R.id.no)
+                    val dialogYes = dialogView.findViewById<TextView>(R.id.yes)
+
+                    builder.setView(dialogView).setNegativeButton("취소"){
+                        dialogInterface, i ->
+                    }.setPositiveButton("확인"){
+                        dialogInterface, i -> Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+                    }.show()
+                }
+            }
+            true
+        })
+
+        popup.show()
     }
 }
--- app/src/main/java/com/seoulapp/startpick/ui/WriteActivity.kt
+++ app/src/main/java/com/seoulapp/startpick/ui/WriteActivity.kt
@@ -3,6 +3,8 @@ package com.seoulapp.startpick.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.isapanah.awesomespinner.AwesomeSpinner
 import com.seoulapp.startpick.R
 import kotlinx.android.synthetic.main.activity_write.*
 
@@ -24,9 +26,58 @@ class WriteActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        btn_company.setOnClickListener{
+        btn_company.setOnClickListener {
             var intent = Intent(this, StartupDivActivity::class.java)
             startActivity(intent)
         }
+
+        JobSpinner()
+        PersonSpinner()
+
+
     }
+
+    /** 스피너 호출 함수 */
+    fun JobSpinner() {
+
+        var spinner = findViewById(R.id.selectJob) as AwesomeSpinner
+
+        var spinnerArray = ArrayList<String>()
+        spinnerArray.add("개발")
+        spinnerArray.add("기획")
+        spinnerArray.add("디자인")
+        spinnerArray.add("마케팅")
+        spinnerArray.add("미디어")
+        spinnerArray.add("영업")
+        spinnerArray.add("기타")
+
+        val categoriesAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray)
+
+        spinner.setAdapter(categoriesAdapter)
+        spinner.setOnSpinnerItemClickListener(
+                AwesomeSpinner.onSpinnerItemClickListener { position, itemAtPosition ->
+                })
+    }
+
+    fun PersonSpinner() {
+
+        var spinner = findViewById(R.id.selectPerson) as AwesomeSpinner
+
+        var spinnerArray = ArrayList<String>()
+        spinnerArray.add("1명")
+        spinnerArray.add("2명")
+        spinnerArray.add("3명")
+        spinnerArray.add("4명")
+        spinnerArray.add("5명")
+        spinnerArray.add("미정")
+
+        val categoriesAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray)
+
+        spinner.setAdapter(categoriesAdapter)
+        spinner.setOnSpinnerItemClickListener(
+                AwesomeSpinner.onSpinnerItemClickListener { position, itemAtPosition ->
+                })
+    }
+
+
 }
